64b911ee99d0fdd77c7e2c2f565e9eb5feae02b9
Omar Katary <omar.katary@hotmail.com>
2020-04-02 16:49:10 +0200

    Added search bar component and map marker

--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -1,26 +1,48 @@
 import React, {Component} from 'react';
 import {
-  View, Text
+  View, Text, StyleSheet
 } from 'react-native';
-import MapView from 'react-native-maps';
+import  MapView, { Marker } from 'react-native-maps';
+import SearchBar from '../components/SearchBar'
 
 class MainPage extends Component{
     render(){
         return(
       
-        <View>
-        <MapView
-        style = {{height : '100%'}}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}/>
-      </View>
+        <View style = {styles.container}>
+          <MapView
+          style = {styles.map}
+          initialRegion={{
+            latitude: 31.1683,
+            longitude: 29.9316,
+            latitudeDelta: 0.00922,
+            longitudeDelta: 0.00421,
+          }}>
+          <Marker
+            coordinate={{
+              latitude: 31.1683,
+              longitude: 29.932}}
+              title="Carrefour's Parking"
+          />
+          </MapView>
+          <SearchBar style = {styles.searchBar}/>
+        
+        </View>
             )
     }
    
 }
+
+const styles = StyleSheet.create({
+  container : {
+    flex : 1,
+  },
+  map : {
+    flex: 1,
+    height : '100%'
+  },
+
+
+})
 
 export default MainPage
