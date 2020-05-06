3ac010ba825b47eea1fdfc35d9de22f127ff0a18
Omar Katary <omar.katary@hotmail.com>
2020-05-06 17:08:37 +0200

    added parking lot component

---
+++ src/components/ParkingLot.js
@@ -0,0 +1,90 @@
+import React, {Component} from 'react';
+import { StyleSheet, View,ScrollView,Text} from 'react-native';
+import ParkingSection from './ParkingSection'
+
+class ParkingLot extends Component{
+    state = {   gridRows: 2,
+                gridColumns:3,
+                pendingSpotExists:false,
+                parkingSections:
+                [   {parkingSectionID: 1,
+                    isParkingSectionHorizontal: false,
+                    isParkingSpotHorizontal: false,
+                    isDoubleSectioned: false,
+                    sectionColumn: 1,
+                    sectionRow: 1,
+                    },
+                    {parkingSectionID: 2,
+                    isParkingSectionHorizontal: false,
+                    isParkingSpotHorizontal: false,
+                    isDoubleSectioned: true,
+                    sectionColumn: 2,
+                    sectionRow: 2,
+                    },
+                    {parkingSectionID: 3,
+                    isParkingSectionHorizontal: true,
+                    isParkingSpotHorizontal: true,
+                    isDoubleSectioned: true,
+                    sectionColumn: 2,
+                    sectionRow: 1,
+                    },
+                    {parkingSectionID: 4,
+                    isParkingSectionHorizontal: true,
+                    isParkingSpotHorizontal: false,
+                    isDoubleSectioned: true,
+                    sectionColumn: 3,
+                    sectionRow: 1,
+                    }
+                ]
+    }
+    setPendingSpotExists = (value) => {
+        this.setState({pendingSpotExists:value})
+    }
+    
+    sectionGenerator = () => {
+        parkingColumns = []
+        sectionViewList = []
+        for(i=1; i<=this.state.gridColumns; i++){
+            this.state.parkingSections.map(section =>{
+                if(section.sectionColumn == i){
+                    sectionViewList.push(<ParkingSection    key={section.parkingSectionID} 
+                                                            isParkingSpotHorizontal={section.isParkingSpotHorizontal} 
+                                                            isParkingSectionHorizontal={section.isParkingSectionHorizontal} 
+                                                            isDoubleSectioned={section.isDoubleSectioned}
+                                                            pendingSpotExists={this.state.pendingSpotExists}
+                                                            setPendingSpotExists={this.setPendingSpotExists}/>)
+                }
+            })
+         
+            parkingColumns.push(<View style={styles.parkingSection}>{sectionViewList.map(section =>{return(section)})}</View>)
+            sectionViewList = []
+        }
+        return parkingColumns
+    }
+
+    render(){
+    columnsList = this.sectionGenerator()
+    return(
+        <ScrollView horizontal= {true} maximumZoomScale={5} minimumZoomScale={0.5} scrollEnabled={true}>
+            <View style={styles.parkingLot}>
+                    {columnsList.map((column)=>{ return column})}
+
+            </View>
+        </ScrollView>
+    )
+    }
+}
+
+const styles = StyleSheet.create({
+
+    parkingSection:{
+        flex:1,
+        margin:20
+    },
+    parkingLot : {
+       flex:1,
+       flexDirection: 'row'
+    }
+  })
+
+export default ParkingLot
