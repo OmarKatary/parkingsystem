0565a77d77e588771b1db9e003de57e927624b88
Omar Katary <omar.katary@hotmail.com>
2020-05-21 00:44:04 +0200

    added redux packages

--- App.js
+++ App.js
@@ -7,14 +7,18 @@
  */
 
 import React from 'react';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './src/reducers'
 import MainPage from './src/containers/MainPage'
 import ParkingPage from './src/containers/ParkingPage'
 import ParkingSection from './src/components/ParkingSection'
 import ParkingLot from './src/components/ParkingLot'
 const App= () => {
   return (
-    // <ParkingSection/>
-    <MainPage/>
+    <Provider store={createStore(reducers)}>
+      <MainPage/>
+    </Provider>
   );
 };
 
